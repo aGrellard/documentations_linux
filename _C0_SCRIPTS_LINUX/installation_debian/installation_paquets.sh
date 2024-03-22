39d97f55816511929ea99b1c59363e241f96e258
aGrellard <grellard.adrien@gmail.com>
2024-03-22 16:52:05 +0100

    update

--- _C0_SCRIPTS_LINUX/installation_debian/installation_paquets.sh
+++ _C0_SCRIPTS_LINUX/installation_debian/installation_paquets.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
-
-echo "Installation des utilitaires"
+echo "Mise à jour des paquets"
 sudo apt update
+echo "Installation des paquets de base"
 sudo apt install -y curl wget git unzip tmux
-
 echo "Installation de zsh"
 sudo apt install -y zsh
-
 sudo chsh -s "$(which zsh)" $USER
-
 echo "Installation de oh-my-zsh"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o install_ohmyzsh.sh
-
 echo "Mise à jour du shell par défaut (ceci nécessitera un mot de passe et une nouvelle connexion pour prendre effet)"
