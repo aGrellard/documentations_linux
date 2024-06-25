15828bf2359f5e51c92ad6b3d92eb501c2dd4990
aGrellard <grellard.adrien0gmail.com>
2024-06-25 18:14:01 +0200

    Deployed 67f9f58 with MkDocs version: 1.6.0

--- javascripts/config.js
+++ javascripts/config.js
@@ -15,3 +15,23 @@ window.MathJax = {
 document$.subscribe(() => {
   MathJax.typesetPromise()
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Sélectionne tous les toggles de la navigation
+  var toggles = document.querySelectorAll('.md-nav__toggle');
+  
+  toggles.forEach(function(toggle) {
+      // Ferme tous les menus par défaut
+      toggle.checked = false;
+
+      // Ajoute un événement de changement pour ouvrir/fermer le menu
+      toggle.addEventListener('change', function() {
+          var nav = toggle.closest('li').querySelector('.md-nav');
+          if (nav) {
+              var expanded = toggle.checked;
+              nav.setAttribute('aria-expanded', expanded.toString());
+          }
+      });
+  });
+});
+
